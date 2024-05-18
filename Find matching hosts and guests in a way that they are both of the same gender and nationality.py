1f7c4499a2ced35d93abd4de3c4e9bd36a8af49b
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2024-05-18 20:18:40 +0530

    Update Find matching hosts and guests in a way that they are both of the same gender and nationality.py

--- Find matching hosts and guests in a way that they are both of the same gender and nationality.py
+++ Find matching hosts and guests in a way that they are both of the same gender and nationality.py
@@ -3,7 +3,9 @@ import pyspark
 from pyspark.sql import functions as f
 
 # Start writing code
-df = airbnb_hosts.join(airbnb_guests, (airbnb_hosts.gender==airbnb_guests.gender) & (airbnb_hosts.nationality==airbnb_guests.nationality), how="inner").distinct().select(airbnb_hosts.host_id, airbnb_guests.guest_id)
+df = airbnb_hosts.join(airbnb_guests, (airbnb_hosts.gender==airbnb_guests.gender) & (airbnb_hosts.nationality==airbnb_guests.nationality), how="inner")
+     .distinct()
+     .select(airbnb_hosts.host_id, airbnb_guests.guest_id)
 
 # To validate your solution, convert your final pySpark df to a pandas df
 df.toPandas()
